571f1bb481e680b651ce96968ba35dc1397af409
Emmanuel Turlay <emmanuel@instacart.com>
2017-10-25 16:40:37 -0700

    Compliance with sklear.base.BaseEstimator for Holt-Winters

--- lore/estimators/holt_winters/__init__.py
+++ lore/estimators/holt_winters/__init__.py
@@ -12,11 +12,20 @@ class HoltWinters(BaseEstimator):
     self.periodicity = kwargs.get('periodicity')
     self.forecasts = kwargs.get('days_to_forecast')
     self.kwargs = kwargs
+    self.params = None
+
+  @timed(logging.INFO)
+  def fit(self, x, y=None):
+    results = holtwinters.additive(x, self.periodicity, self.forecasts, 
+      alpha=self.kwargs.get('alpha'),
+      beta=self.kwargs.get('beta'),
+      gamma=self.kwargs.get('gamma'))
+    self.params = {'alpha': results[1], 'beta': results[2], 'gamma': results[3]}
+    self.rmse = results[4]
+    return {'alpha': results[1], 'beta': results[2], 'gamma': results[3], 'RMSE': self.rmse}
 
   @timed(logging.INFO)
   def predict(self, X):
     return holtwinters.additive(X, self.periodicity, self.forecasts, 
-      alpha=self.kwargs.get('alpha'),
-      beta=self.kwargs.get('beta'),
-      gamma=self.kwargs.get('gamma'))
+      **self.params)[0]
     
