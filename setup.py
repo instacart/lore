9d9ee6a375733218dc1a44694cab2e01a0300875
Montana Low <montana@instacart.com>
2017-11-01 18:09:39 -0700

    lock keras and xgboost as well

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ postgres = ['psycopg2', 'sqlalchemy==1.2.0b3', 'sqlalchemy-migrate']
 redis = ['redis']
 s3 = ['boto3']
 
-keras = ['keras', 'tensorflow<1.4', 'dill', 'h5py']
-xgboost = ['xgboost']
+keras = ['Keras==2.0.8', 'tensorflow==1.3.0', 'dill', 'h5py==2.7.0']
+xgboost = ['xgboost==0.6a2']
 scikit = ['scikit-learn']
 
 all = keras + xgboost + scikit + postgres + redis
