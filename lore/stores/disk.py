f4fc9b32df57f108b3d270a5f3bfd903efa2de92
Montana Low <montana@instacart.com>
2017-11-22 19:06:20 -0600

    test new cache eviction logic

--- lore/stores/disk.py
+++ lore/stores/disk.py
@@ -26,10 +26,10 @@ class Disk(Base):
             with open(self._path(key), 'wb') as f:
                 pickle.dump(value, f)
 
-        if os.path.getsize(key) > self.limit:
-            raise MemoryError('disk cache limit exceeded by single key: %s' % key)
-        
         if self.limit is not None:
+            if os.path.getsize(self._path(key)) > self.limit:
+                raise MemoryError('disk cache limit exceeded by single key: %s' % key)
+    
             with timer('evict: %s' % key):
                 while self.size() > self.limit:
                     del self[self.lru()]
