8efe1ab9edd87623b73c98460b927addfa3b797b
Montana Low <montana@instacart.com>
2018-02-27 16:26:16 -0800

    ensure query cache limit from env is int

--- lore/template/init/app/__init__.py
+++ lore/template/init/app/__init__.py
@@ -3,4 +3,4 @@ import lore.stores
 import os
 
 lore.env.project = __name__
-lore.stores.query_cache.limit = os.environ.get('LORE_QUERY_CACHE_LIMIT', 10000000000)
+lore.stores.query_cache.limit = int(os.environ.get('LORE_QUERY_CACHE_LIMIT', 10000000000))
