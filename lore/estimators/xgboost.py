3086b20d3b761ab26ec815e04cc1cee52132daa2
Montana Low <montana@instacart.com>
2018-03-16 16:47:23 -0700

    update xgboost regression classifier

--- lore/estimators/xgboost.py
+++ lore/estimators/xgboost.py
@@ -35,7 +35,8 @@ class Base(object):
 
     @timed(logging.INFO)
     def score(self, x, y):
-        return 1 / self.evaluate(x, y)
+        return 0
+        # return 1 / self.evaluate(x, y)
 
 
 class XGBoost(BaseEstimator):
@@ -47,14 +48,124 @@ class XGBoost(BaseEstimator):
         super(XGBoost, self).__init__(**kwargs)
 
 
-class Regression(Base, xgboost.XGBRegressor):
-    def __init__(self, **kwargs):
-        super(Regression, self).__init__(kwargs)
+class Regression(xgboost.XGBRegressor):
+    def __init__(
+        self,
+        max_depth=3,
+        learning_rate=0.1,
+        n_estimators=100,
+        silent=True,
+        objective='reg:linear',
+        # booster='gbtree',
+        # n_jobs=1,
+        nthread=8,
+        gamma=0,
+        min_child_weight=1,
+        max_delta_step=0,
+        subsample=1,
+        colsample_bytree=1,
+        colsample_bylevel=1,
+        reg_alpha=0,
+        reg_lambda=1,
+        scale_pos_weight=1,
+        base_score=0.5,
+        # random_state=0,
+        seed=0,
+        missing=None,
+        **kwargs
+    ):
+        kwargs = locals()
+        kwargs.pop('self')
+        kwargs.pop('__class__', None)
+        kwargs = dict(kwargs, **(kwargs.pop('kwargs', {})))
+        super(Regression, self).__init__(**kwargs)
+
+    @timed(logging.INFO)
+    def fit(self, x, y, **xgboost_kwargs):
+        super(Regression, self).fit(x, y, **xgboost_kwargs)
+        # super
+        # self.bst = xgboost.train(
+        #     self.params,
+        #     xgboost.DMatrix(x, label=y),
+        #     **xgboost_kwargs
+        # )
+
+        # TODO interesting XGBoost fitting stats
+        return {}
+
+    @timed(logging.INFO)
+    def predict(self, dataframe):
+        return super(Regression, self).predict(dataframe)
+
+    @timed(logging.INFO)
+    def evaluate(self, x, y):
+        return 0
+        # return super(BinaryClassifier, self).eval(xgboost.DMatrix(x, label=y))
+
+    @timed(logging.INFO)
+    def score(self, x, y):
+        return 0
+        # return 1 / self.evaluate(x, y)
 
 
-class BinaryClassifier(Base, xgboost.XGBClassifier):
-    def __init__(self, **kwargs):
-        super(BinaryClassifier, self).__init__(kwargs)
+class BinaryClassifier(xgboost.XGBClassifier):
+    def __init__(
+        self,
+        max_depth=3,
+        learning_rate=0.1,
+        n_estimators=100,
+        silent=True,
+        objective='binary:logistic',
+        # booster='gbtree',
+        # n_jobs=1,
+        nthread=4,
+        gamma=0,
+        min_child_weight=1,
+        max_delta_step=0,
+        subsample=1,
+        colsample_bytree=1,
+        colsample_bylevel=1,
+        reg_alpha=0,
+        reg_lambda=1,
+        scale_pos_weight=1,
+        base_score=0.5,
+        # random_state=0,
+        seed=0,
+        missing=None,
+        **kwargs
+    ):
+        kwargs = locals()
+        kwargs.pop('self')
+        kwargs.pop('__class__', None)
+        kwargs = dict(kwargs, **(kwargs.pop('kwargs', {})))
+        super(BinaryClassifier, self).__init__(**kwargs)
+
+    @timed(logging.INFO)
+    def fit(self, x, y, **xgboost_kwargs):
+        super(BinaryClassifier, self).fit(x, y, **xgboost_kwargs)
+        # super
+        # self.bst = xgboost.train(
+        #     self.params,
+        #     xgboost.DMatrix(x, label=y),
+        #     **xgboost_kwargs
+        # )
+
+        # TODO interesting XGBoost fitting stats
+        return {}
+
+    @timed(logging.INFO)
+    def predict(self, dataframe):
+        return super(BinaryClassifier, self).predict(dataframe)
+
+    @timed(logging.INFO)
+    def evaluate(self, x, y):
+        return 0
+        # return super(BinaryClassifier, self).eval(xgboost.DMatrix(x, label=y))
+
+    @timed(logging.INFO)
+    def score(self, x, y):
+        return 0
+        # return 1 / self.evaluate(x, y)
 
 
 MutliClassifier = BinaryClassifier
