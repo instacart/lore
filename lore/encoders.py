9ef25408f16281417069646f1d0d5f6b5c114b8d
ganesh_krishnan <ganesh.krishnan@instacart.com>
2018-07-05 20:47:29 -0700

    Add OneHot and CompressedOneHot encodings
    Improve implementation of CompressedOneHot
    
    Remove OneHot constructor

--- lore/encoders.py
+++ lore/encoders.py
@@ -9,10 +9,9 @@ import inflection
 import numpy
 import pandas
 from smart_open import smart_open
-
 import lore
 import lore.transformers
-from lore.util import timer
+from lore.util import timer, get_relevant_args
 
 logger = logging.getLogger(__name__)
 TWIN = '_twin'
@@ -562,6 +561,73 @@ class Unique(Base):
     def cardinality(self):
         # 1 for tail value, 1 for missing_value, and 1 for preserving 0
         return len(self.map) + 3
+
+
+class OneHot(Base):
+    """
+    Performs one hot encoding
+    """
+    def fit(self, data):
+        with timer(('fit one-hot %s:' % self.name), logging.DEBUG):
+            self.dummy_columns = self.get_dummies(data).columns.values
+            self.sequence_length = len(self.dummy_columns)
+
+    def get_dummies(self, data):
+        data = self.series(data)
+        return pandas.get_dummies(data, prefix=self.column)
+
+    def transform(self, data):
+        with timer('transform one_hot %s:' % self.name, logging.DEBUG):
+            dummies = self.get_dummies(data)
+            for col in [c for c in self.dummy_columns if c not in dummies.columns]:
+                dummies[col] = 0
+            return dummies[self.dummy_columns]
+
+    def get_column(self, encoded, i):
+        dummy = self.dummy_columns[i]
+        if dummy in encoded:
+            return encoded[dummy]
+        else:
+            return pandas.Series([0] * len(encoded))
+
+    def reverse_transform(self, data): pass
+
+    def cardinality(self):
+        return self.sequence_length
+
+    def sequence_name(self, i, suffix=''):
+        return (self.name + '_%i' + suffix) % i
+
+
+class CompressedOneHot(Unique):
+    """Performs a compressed one hot encoding. Data is first passed through
+       the Unique encoder to prune infrequent values (check documention for Unique).
+       Then a one hot encoding is performed"""
+    def __init__(self, *args, **kwargs):
+        uniq_kwargs = get_relevant_args(Unique.__init__, kwargs)
+        ohe_kwargs = get_relevant_args(OneHot.__init__, kwargs)
+        super(CompressedOneHot, self).__init__(*args, **uniq_kwargs)
+        self.ohe = OneHot(*args, **ohe_kwargs)
+
+    def fit(self, data):
+        super(CompressedOneHot, self).fit(data)
+        uniqued = super(CompressedOneHot, self).transform(data)
+        df = pandas.DataFrame({self.column: uniqued}, index=data.index)
+        self.ohe.fit(df)
+        self.sequence_length = self.ohe.sequence_length
+
+    def transform(self, data):
+        series = super(CompressedOneHot, self).transform(data)
+        df = pandas.DataFrame({self.column: series}, index=data.index)
+        return self.ohe.transform(df)
+
+    def reverse_transform(self, data): pass
+
+    def cardinality(self):
+        try:
+            return self.ohe.cardinality()
+        except AttributeError:
+            return super(CompressedOneHot, self).cardinality()
 
 
 class Token(Unique):
