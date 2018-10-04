9e5b5687dfc10e5692ad76c7ac808f2b51083916
ganesh_krishnan <ganesh.krishnan@instacart.com>
2018-10-04 16:19:11 -0700

    Add memoized property to utils

--- lore/util.py
+++ lore/util.py
@@ -421,3 +421,46 @@ def convert_df_columns_to_json(df, columns):
     series = pandas.Series(df[columns].to_dict(orient='records'), index=df.index)
     series = series.apply(lambda x: json.dumps(x, default=str))
     return series
+
+
+def sql_alchemy_object_as_dict(obj):
+    from sqlalchemy import inspect
+    return {c.key: getattr(obj, c.key)
+            for c in inspect(obj).mapper.column_attrs}
+
+
+# From memoized_property package, copied source so as to prevent adding dependencies
+def memoized_property(fget):
+    """
+    Return a property attribute for new-style classes that only calls its getter on the first
+    access. The result is stored and on subsequent accesses is returned, preventing the need to
+    call the getter any more.
+    Example::
+        >>> class C(object):
+        ...     load_name_count = 0
+        ...     @memoized_property
+        ...     def name(self):
+        ...         "name's docstring"
+        ...         self.load_name_count += 1
+        ...         return "the name"
+        >>> c = C()
+        >>> c.load_name_count
+        0
+        >>> c.name
+        "the name"
+        >>> c.load_name_count
+        1
+        >>> c.name
+        "the name"
+        >>> c.load_name_count
+        1
+    """
+    attr_name = '_{0}'.format(fget.__name__)
+
+    @functools.wraps(fget)
+    def fget_memoized(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fget(self))
+        return getattr(self, attr_name)
+
+    return property(fget_memoized)
