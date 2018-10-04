ce1b251698e39c7242d190ab2838ec9bad22a12b
ganesh_krishnan <ganesh.krishnan@instacart.com>
2018-10-04 16:19:31 -0700

    Add basic unit test for fit metadata

---
+++ tests/unit/test_metadata.py
@@ -0,0 +1,42 @@
+import unittest
+import tests.mocks.models
+import lore.metadata
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+Session = scoped_session(sessionmaker(bind=lore.io.metadata._engine))
+
+
+def truncate_table(table_name):
+    lore.io.metadata.execute('TRUNCATE {} RESTART IDENTITY CASCADE'.format(table_name))
+
+
+def truncate_metadata_tables():
+    truncate_table('fittings')
+    truncate_table('commits')
+    truncate_table('predictions')
+    truncate_table('snapshots')
+
+
+def setUpModule():
+    truncate_metadata_tables()
+
+
+def tearDownModule():
+    truncate_metadata_tables()
+
+
+class FitLogging(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = tests.mocks.models.XGBoostRegressionWithPredictionLogging()
+        cls.df = cls.model.pipeline.training_data
+
+    def test_basic_logging_(self):
+        self.model.fit()
+        session = Session()
+        model_metadata = session.query(lore.metadata.Fitting).filter_by(model=self.model.name).first()
+        session.close()
+        self.assertIsNotNone(model_metadata)
+
+    def test_fit_number_from_db(self):
+        pass
