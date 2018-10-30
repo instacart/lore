57488c13c1eff5bdb119fa82cc220ae2d3263543
ganesh_krishnan <ganesh.krishnan@instacart.com>
2018-10-29 17:51:37 -0700

    Add unit tests for naive model

--- tests/mocks/models.py
+++ tests/mocks/models.py
@@ -4,9 +4,11 @@ import lore.models
 import lore.models.keras
 import lore.models.sklearn
 import lore.models.xgboost
+import lore.models.naive
 import lore.estimators.keras
 import lore.estimators.sklearn
 import lore.estimators.xgboost
+import lore.estimators.naive
 import tests.mocks.pipelines
 
 
@@ -188,6 +190,37 @@ class OneHotBinaryClassifier(lore.models.xgboost.Base):
         super(OneHotBinaryClassifier, self).__init__(
             tests.mocks.pipelines.OneHotPipeline(),
             lore.estimators.xgboost.BinaryClassifier())
+
+    def before_fit(self, *args, **kwargs):
+        self.called_before_fit = True
+
+    def after_fit(self, *args, **kwargs):
+        self.called_after_fit = True
+
+    def before_predict(self, *args, **kwargs):
+        self.called_before_predict = True
+
+    def after_predict(self, *args, **kwargs):
+        self.called_after_predict = True
+
+    def before_evaluate(self, *args, **kwargs):
+        self.called_before_evaluate = True
+
+    def after_evaluate(self, *args, **kwargs):
+        self.called_after_evaluate = True
+
+    def before_score(self, *args, **kwargs):
+        self.called_before_score = True
+
+    def after_score(self, *args, **kwargs):
+        self.called_after_score = True
+
+
+class NaiveBinaryClassifier(lore.models.naive.Base):
+    def __init__(self):
+        super(NaiveBinaryClassifier, self).__init__(
+            tests.mocks.pipelines.NaivePipeline(),
+            lore.estimators.naive.BinaryClassifier())
 
     def before_fit(self, *args, **kwargs):
         self.called_before_fit = True
--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -2,6 +2,7 @@ import unittest
 
 import tests.mocks.models
 import scipy.stats
+import numpy
 
 
 class TestKeras(unittest.TestCase):
@@ -221,3 +222,38 @@ class TestOneHotBinaryClassifier(unittest.TestCase):
         self.assertTrue(model.called_before_score)
         self.assertTrue(model.called_after_score)
 
+
+class TestNaiveBinaryClassifier(unittest.TestCase):
+    def test_lifecycle(self):
+        model = tests.mocks.models.NaiveBinaryClassifier()
+        model.fit()
+        model.save()
+
+        loaded = tests.mocks.models.NaiveBinaryClassifier.load()
+        self.assertEqual(loaded.fitting, model.fitting)
+
+    def test_before_after_hooks(self):
+        model = tests.mocks.models.NaiveBinaryClassifier()
+        model.fit(test=True, score=True)
+        model.predict(model.pipeline.test_data)
+
+        self.assertTrue(model.called_before_fit)
+        self.assertTrue(model.called_after_fit)
+        self.assertTrue(model.called_before_predict)
+        self.assertTrue(model.called_after_predict)
+        self.assertTrue(model.called_before_evaluate)
+        self.assertTrue(model.called_after_evaluate)
+        self.assertTrue(model.called_before_score)
+        self.assertTrue(model.called_after_score)
+
+    def test_preds(self):
+        model = tests.mocks.models.NaiveBinaryClassifier()
+        model.fit(test=True, score=True)
+        preds = model.predict(model.pipeline.test_data)
+        self.assertTrue((preds == 1).all())
+
+    def test_probs(self):
+        model = tests.mocks.models.NaiveBinaryClassifier()
+        model.fit(test=True, score=True)
+        probs = model.predict_proba(model.pipeline.test_data)
+        self.assertTrue((numpy.abs(probs - 0.667) < 0.001).all())
