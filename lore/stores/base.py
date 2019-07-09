d7d2e8ab543d89c03e746434ebc05d5448a8258c
Diego Maniloff <diego.maniloff@gmail.com>
2019-07-09 12:05:53 -0400

    need to include the query params too.

--- lore/stores/base.py
+++ lore/stores/base.py
@@ -3,6 +3,8 @@ from abc import ABCMeta, abstractmethod
 import hashlib
 import inspect
 
+import sqlalchemy
+
 
 class Base(object):
     __metaclass__ = ABCMeta
@@ -47,14 +49,17 @@ class Base(object):
         stack = inspect.stack()
         caller = kwargs.pop('caller', stack[-2])
         instance = kwargs.pop('instance', self)
-        query_string = (
-            '__'.join(map(str, args)).encode('utf-8') +
-            str(kwargs).encode('utf-8')
-        )
+        if isinstance(args[0], sqlalchemy.sql.Selectable):
+            query_string = (
+                str(args[0]).encode('utf-8') +
+                str(args[0].compile().params).encode('utf-8')
+            )
+        else:
+            query_string = str(args).encode('utf-8')
 
         return '.'.join((
             instance.__module__,
             instance.__class__.__name__,
             caller.__code__.co_name,
-            hashlib.sha1(query_string).hexdigest()
+            hashlib.sha1(query_string + str(kwargs).encode('utf-8')).hexdigest()
         ))
