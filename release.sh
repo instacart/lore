b4cf7451c8188dd77d3fdf33d87535d464ef8ee5
Marco Montagna <marco.montagna@instacart.com>
2020-02-05 10:28:25 -0800

    Add script to do releases. (#157)

---
+++ release.sh
@@ -0,0 +1,14 @@
+  
+#!/bin/bash
+
+TAG=v`cat lore/__init__.py | grep  '__version__ ='  | awk '{ print $3}'`
+
+git tag $TAG
+git push origin $TAG
+
+rm -r build/ dist/ ./*.egg-info/
+
+python setup.py sdist bdist_wheel
+
+twine upload -r pypi dist/*
+
